55c55ea6bf6bc7e73cb40edf5b6a85dd9e8f75ef
Diaszano <lucasdiiassantos@gmail.com>
2022-06-08 18:26:55 -0300

    add novos app, depois vou tirar redundancias

--- install.sh
+++ install.sh
@@ -7,6 +7,105 @@ sudo apt-get dist-upgrade -y
 # ---------------------------------------------------
 clear
 # ---------------------------------------------------
+# Discord - https://discord.com/
+# ---------------------------------------------------
+nome=discord
+pacote=$(dpkg --get-selections | grep "$nome" ) 
+if [ -n "$pacote" ] ;
+then echo
+    echo "$nome ja esta instalado!"
+    sleep 5
+else echo
+    echo "Instalando $nome"
+    sudo apt update -y
+    sudo apt upgrade -y
+    wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
+    sudo apt install ./discord.deb -y
+    echo "Instalacao do $nome concluida"
+    sleep 5
+fi
+# ---------------------------------------------------
+clear
+# ---------------------------------------------------
+# Spotify - https://www.spotify.com/br/
+# ---------------------------------------------------
+nome=spotify
+pacote=$(dpkg --get-selections | grep "$nome" ) 
+if [ -n "$pacote" ] ;
+then echo
+    echo "$nome ja esta instalado!"
+    sleep 5
+else echo
+    echo "Instalando $nome"
+    curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+    echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+    sudo apt-get update -y 
+    sudo apt-get install $nome-client -y
+    echo "Instalacao do $nome concluida"
+    sleep 5
+fi
+# ---------------------------------------------------
+clear
+# ---------------------------------------------------
+# Google Chrome - https://www.google.com/intl/pt-BR/chrome/
+# ---------------------------------------------------
+nome=google-chrome
+pacote=$(dpkg --get-selections | grep "$nome" ) 
+if [ -n "$pacote" ] ;
+then echo
+    echo "$nome ja esta instalado!"
+    sleep 5
+else echo
+    echo "Instalando $nome"
+    wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+    sudo apt-get update -y
+    sudo apt install $nome-stable -y
+    echo "Instalacao do $nome concluida"
+    sleep 5
+fi
+# ---------------------------------------------------
+clear
+# ---------------------------------------------------
+# DBeaver - https://dbeaver.io/
+# ---------------------------------------------------
+nome=dbeaver
+pacote=$(dpkg --get-selections | grep "$nome" ) 
+if [ -n "$pacote" ] ;
+then echo
+    echo "$nome ja esta instalado!"
+    sleep 5
+else echo
+    echo "Instalando $nome"
+    wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
+    sudo dpkg -i dbeaver-ce_latest_amd64.deb
+    echo "Instalacao do $nome concluida"
+    sleep 5
+fi
+# ---------------------------------------------------
+clear
+# ---------------------------------------------------
+# Visual Studio Code - https://code.visualstudio.com/
+# ---------------------------------------------------
+nome=code
+pacote=$(dpkg --get-selections | grep "$nome" ) 
+if [ -n "$pacote" ] ;
+then echo
+    echo "$nome ja esta instalado!"
+    sleep 5
+else echo
+    echo "Instalando $nome"
+    sudo apt update
+    sudo apt install software-properties-common apt-transport-https wget
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt-get install $nome -y
+    echo "Instalacao do $nome concluida"
+    sleep 5
+fi
+# ---------------------------------------------------
+clear
+# ---------------------------------------------------
 # PIP - https://pypi.org/project/pip/
 # ---------------------------------------------------
 nome=pip
@@ -125,6 +224,12 @@ else echo
     echo "Instalacao do $nome concluida"
     sleep 5
 fi
+# ---------------------------------------------------
+clear
+# ---------------------------------------------------
+sudo apt-get update -y
+sudo apt-get upgrade -y 
+sudo apt-get dist-upgrade -y 
 # ---------------------------------------------------
 clear
 # ---------------------------------------------------
